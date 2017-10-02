b9a470dca1d78da0b53be868223640ff05c0bc05
aarnavPython <32436689+aarnavPython@users.noreply.github.com>
2017-10-02 16:36:16 +0530

    Create simple_age_calc.py

---
+++ Age_Calculator/simple_age_calc.py
@@ -0,0 +1,17 @@
+print ("Welcome to the simple age meter. Just fill up a few things and you're good to go!")
+age = int(input("Please enter your age: "))
+name = (input("Please enter your name: "))
+day = age * 365
+minutes = age * 525600
+seconds = age * 31536000
+print (name, "has been alive for", day, "days,", minutes,"minutes,",seconds,"seconds! Wow!")
+
+'''
+This program is the easiest actual working program you can find. It uses basics like:
+print, input, integer input, variable, printing variables with strings etc.
+Definitely easier than a calculator which is a little bit more. I have made a calculator which
+has like 50 lines of code, where as this just has about 7. So, refer to this and see where you
+are as a beginner.
+'''
+
+
